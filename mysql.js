937e3fdf3c07514af7353b9a6960f6fcdc82bf18
chavad <chava7993@gmail.com>
2019-11-04 13:11:40 +0200

    Connect to mysql + ejs

---
+++ mysql.js
@@ -0,0 +1,26 @@
+const mysql = require('promise-mysql');
+
+let db;
+
+mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.MYSQL_URL,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
+
+
+}).then((c) => {
+    db = c;
+    db.query("select * from usersname")
+}).catch((e) => {
+    console.error(e);
+});
+
+
+
+module.exports = async function () {
+    let promise = db.query("select * from usersname");
+    let data = await promise;
+    return data;
+}
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const registration = require('./registration.js');
 const app = express();
+const mysql = require('./mysql.js');
 const port = process.env.PORT || 80;
+
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(cookieParser());
@@ -23,7 +28,19 @@ app.get('/login', (req, res) => res.sendFile('./pages/loginpage.html', {
 app.get('/about', (req, res) => res.sendFile('./pages/about.html', {
     root: __dirname
 }));
+// let user = ['chava', 'adva'];
+// app.get('/userInfo/:id', (req, res) => {
+//     console.log('req.params.id');
+//     res.render('pages/userInfo', {
+//         users: user[req.params.id],
+//         id: req.params.id
+//     });
+// });
 
+app.get('/mysql', async function (req, res) {
+    let data = await mysql();
+    res.send(data);
+});
 
 app.post('/registration/register', (req, res) => {
     return registration.register(req, res);
